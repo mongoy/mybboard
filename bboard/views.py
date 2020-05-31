253119b6149902609d6b108f35ffccb01f8f97de
mongoy@rambler.ru <iwb1966y>
2020-05-31 23:38:01 +0900

    Create templates: base and index html files. Renrer informations from model Bb.

--- bboard/views.py
+++ bboard/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
 from .models import Bb
 
 
@@ -7,7 +9,11 @@ def index(request):
     # """Страница в разработке"""
     # return HttpResponse('404. Страница в разработке')
     """"Простой вывод объявлений"""
-    s = 'Список объявлений:\r\n\r\n\r\n'
-    for bb in Bb.objects.order_by('-published'):
-        s += bb.title + '\r\n' + bb.content + '\r\n\r\n'
-    return HttpResponse(s, content_type='text/plain; charset=utf8')
+    # s = 'Список объявлений:\r\n\r\n\r\n'
+    #     # for bb in Bb.objects.order_by('-published'):
+    #     #     s += bb.title + '\r\n' + bb.content + '\r\n\r\n'
+    #     # return HttpResponse(s, content_type='text/plain; charset=utf8')
+    template = loader.get_template('bboard/index.html')
+    bbs = Bb.objects.order_by('-published')
+    context = {'bbs': bbs}
+    return HttpResponse(template.render(context, request))
